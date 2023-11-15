1d391b0abaef32121d1c0a33f572509016adcae3
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-11-15 18:10:20 +0100

    Sending all products from data.json

--- backend/models/Producto.php
+++ backend/models/Producto.php
@@ -1,5 +1,54 @@
 <?php
 class Producto
 {
+    public $id;
+    public $name;
+    public $price;
+    public $description;
+    public function __construct($id, $name, $price, $description)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+    }
 
+    // getters 
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    // setters 
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+    public function getAllInfoProduct(){
+        return array("id"=>$this->id, "name"=>$this->name, "price"=>$this->price, "description"=>$this->description);
+    }
 }
